36bed9fbc68b2fe2da93e52de11b956d6000af81
Zahra-python <zahrabakhtiarinejad22@gmail.com>
2026-08-27 19:57:05 +0300

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,24 @@
+// Define pin numbers for your ESP32 setup
+const int LED1 = 4;
+const int LED2 = 23;
+const int BUTTON = 4;
+
+void setup() {
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
+}
+
+void loop() {
+  int buttonState = digitalRead(BUTTON); // Added missing semicolon
+
+  if (buttonState == LOW) {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+  } else {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+  } 
+}
+
+
